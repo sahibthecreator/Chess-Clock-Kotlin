49115310a2e838acf07d7768db4326b72c31e9f6
Sahib <sahibzulfigar4@gmail.com>
2023-06-19 00:12:40 +0200

    Restart Btn + Time Menu added

--- app/src/main/java/com/example/chessclock/MainActivity.kt
+++ app/src/main/java/com/example/chessclock/MainActivity.kt
@@ -6,8 +6,10 @@ import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,19 +24,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ConstraintLayout>(R.id.timeSettingsMenu).visibility = View.GONE
 
         player1TimerView = findViewById<TextView>(R.id.player1TimerView)
         player2TimerView = findViewById<TextView>(R.id.player2TimerView)
 
         findViewById<Button>(R.id.changePlayer1Time).setOnClickListener{
-            findViewById<LinearLayout>(R.id.timePickerPlayer1).visibility = View.VISIBLE
+            findViewById<ConstraintLayout>(R.id.timeSettingsMenu).visibility = View.VISIBLE
         }
+        findViewById<Button>(R.id.saveTimeBtn).setOnClickListener{
+            findViewById<ConstraintLayout>(R.id.timeSettingsMenu).visibility = View.GONE
+        }
+        findViewById<ImageView>(R.id.restartBtn).setOnClickListener{
+            resetTimer()
+        }
+
 
         player1TimerView!!.setText(String.format("%02d:%02d", player1Time / 1000 / 60, (player1Time / 1000) % 60))
         player2TimerView!!.setText(String.format("%02d:%02d", player2Time / 1000 / 60, (player2Time / 1000) % 60))
 
         val screenLayout = findViewById<LinearLayout>(R.id.screenLayout)
         screenLayout.setOnClickListener {
+            findViewById<TextView>(R.id.pressToStartText).visibility = View.INVISIBLE
+            findViewById<Button>(R.id.changePlayer1Time).visibility = View.INVISIBLE
             isPlayer1Turn = !isPlayer1Turn
             startTimer()
         }
@@ -47,9 +59,9 @@ class MainActivity : AppCompatActivity() {
         val remainingTime = if (isPlayer1Turn) player1Time else player2Time
         val timerTextView : TextView = if (isPlayer1Turn) player1TimerView!! else player2TimerView!!
 
+        var lastTickTime = System.currentTimeMillis()
 
         currentTimer = object : CountDownTimer(remainingTime, 100) {
-            var lastTickTime = System.currentTimeMillis()
 
             override fun onTick(millisUntilFinished: Long) {
                 val currentTime = System.currentTimeMillis()
@@ -59,12 +71,12 @@ class MainActivity : AppCompatActivity() {
                 val seconds = (millisUntilFinished / 1000) % 60
 
                 val formattedTime = String.format("%02d:%02d", minutes, seconds)
-                updatePlayerTime(formattedTime);
+                updatePlayerTime(millisUntilFinished);
                 //timerTextView.text = formattedTime
                 if (isPlayer1Turn)
-                    player1Time -= elapsedTime
+                    player1Time -= 100
                 else
-                    player2Time -= elapsedTime
+                    player2Time -= 100
             }
 
             override fun onFinish() {
@@ -85,9 +97,29 @@ class MainActivity : AppCompatActivity() {
         player2Time = 10000
         isPaused = true
         isPlayer1Turn = false
+        currentTimer?.cancel() // to stop previous players timer
+        resetPlayerTime(player1Time)
     }
 
-    fun updatePlayerTime(formattedTime: String) {
+    fun resetPlayerTime(millis: Long){
+        val minutes = millis / 1000 / 60
+        val seconds = (millis / 1000) % 60
+
+        val formattedTime = String.format("%02d:%02d", minutes, seconds)
+        var player1TimerView = findViewById<TextView>(R.id.player1TimerView)
+        player1TimerView.text = formattedTime;
+        var player2TimerView = findViewById<TextView>(R.id.player2TimerView)
+        player2TimerView.text = formattedTime;
+        findViewById<TextView>(R.id.pressToStartText).visibility = View.VISIBLE
+        findViewById<Button>(R.id.changePlayer1Time).visibility = View.VISIBLE
+    }
+
+
+    fun updatePlayerTime(millis: Long) {
+        val minutes = millis / 1000 / 60
+        val seconds = (millis / 1000) % 60
+
+        val formattedTime = String.format("%02d:%02d", minutes, seconds)
         if(isPlayer1Turn){
             var player1TimerView = findViewById<TextView>(R.id.player1TimerView)
             player1TimerView.text = formattedTime;
